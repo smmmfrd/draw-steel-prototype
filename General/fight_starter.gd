b4b7bd74c2cc1203f5a98ec2d1053cfe06507bca
Sam Mumford <smmmfrd@gmail.com>
2025-01-14 14:23:41 -0800

    Logic: Bug fix, player control assigned enemies

--- General/fight_starter.gd
+++ General/fight_starter.gd
@@ -4,8 +4,8 @@ func _ready() -> void:
 	var player_control : Player_Control = %"Player Group"
 	var enemy_control : Enemy_Control = %"Enemy Group"
 	
-	var units = player_control.get_units()
-	units.append_array(enemy_control.get_units())
+	var units = player_control.get_units().duplicate()
+	units.append_array(enemy_control.get_units().duplicate())
 	
 	var battle = Battle.new(player_control, enemy_control, units)
 	
