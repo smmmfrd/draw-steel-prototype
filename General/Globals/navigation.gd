f009ade484e3fa59a5ceb5b9159ae8af32b36ee4
Sam Mumford <smmmfrd@gmail.com>
2025-01-15 23:04:12 -0800

    Logic: Beginning to work on navigation
    We decided to go back to the drawing board.

--- General/Globals/navigation.gd
+++ General/Globals/navigation.gd
@@ -1,1 +1,23 @@
 extends Node
+
+var map : AStar2D
+var map_size : Vector2i
+
+func init_level(tilemap : TileMapLayer, units: Array[Unit]) -> void:
+	map_size = tilemap.get_used_rect().size
+	var collision = tilemap.get_used_cells()
+	
+	map = AStar2D.new()
+	
+	map.reserve_space(map_size.x * map_size.y)
+	
+	for x in map_size.x:
+		for y in map_size.y:
+			var id = get_map_cell_id(Vector2(x,y))
+			map.add_point(id, tilemap.map_to_local(Vector2(x,y)))
+	
+	print('i cooka da map ', map)
+
+# Convert tilemap location to astar index, apparently it counts up on the y axis, so we mutliply the cell's x by the map's height...
+func get_map_cell_id(cell : Vector2) -> int:
+	return int(cell.y + cell.x * map_size.y)
--- General/fight_starter.gd
+++ General/fight_starter.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var map_collision: TileMapLayer = %"Map Collision"
+
 func _ready() -> void:
 	var player_control : Player_Control = %"Player Group"
 	var enemy_control : Enemy_Control = %"Enemy Group"
@@ -8,5 +10,7 @@ func _ready() -> void:
 	units.append_array(enemy_control.get_units().duplicate())
 	
 	var battle = Battle.new(player_control, enemy_control, units)
+	
+	Navigation.init_level(map_collision, units)
 	
 	FightManager.start_battle(battle)
